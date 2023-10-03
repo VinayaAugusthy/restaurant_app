8d1106f7d735ce30cc7ef5ea7238dd0698c6de83
VinayaAugusthy <vinayaaugusthy@gmail.com>
2023-10-03 16:46:07 +0530

    architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:restaurant_app/home_screen.dart';
+import 'package:restaurant_app/view/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
