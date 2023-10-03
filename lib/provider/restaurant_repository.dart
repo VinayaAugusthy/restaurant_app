0a923d7d725ac8f8e4c9f391dd022ff10aae70c2
VinayaAugusthy <vinayaaugusthy@gmail.com>
2023-10-03 16:58:23 +0530

    api integrated

---
+++ lib/provider/restaurant_repository.dart
@@ -0,0 +1,27 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:restaurant_app/models/restaurant_model.dart';
+
+class RestaurantRepository {
+  Future<RestaurantModel> getRestaurantDetails(String restaurantKey) async {
+    const apiUrl = 'https://kezel.co/api/getAllDigitalMenu.php';
+
+    // Create the request body with the restaurant key
+    final requestBody = {'key': restaurantKey};
+
+    try {
+      final response = await http.post(Uri.parse(apiUrl), body: requestBody);
+
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        final restaurant = RestaurantModel.fromJson(jsonData);
+        return restaurant;
+      } else {
+        throw Exception('Failed to load restaurant details');
+      }
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
+  }
+}
